95a92c8179397bc909982481dc8f42b6603b0f75
Aditya Shekhar <aditya.shekhar9@byjus.com>
2023-03-02 01:55:43 +0530

    Added Cors

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,11 +1,35 @@
+import Cors from "cors";
 import clientPromise from "../../../lib/mongodb";
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtSecret =
   "9c1fbba1a00fe74ba0578553c001c7c6f55ad731addd727c40e03a1fe4cc4d832aa527";
 
+const env = process.env.NODE_ENV;
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+  origin:
+    env === "production"
+      ? "https://this-is-me-74cbf.web.app"
+      : "http://localhost:3000",
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
 export default async function login(req, res) {
   try {
+    await runMiddleware(req, res, cors);
     const { email = "", password = "" } = req.body;
     if (!password || !email) {
       return res.status(400).json({ message: "Invalid input." });
--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,20 +1,42 @@
+import Cors from "cors";
 import clientPromise from "../../../lib/mongodb";
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtSecret =
   "9c1fbba1a00fe74ba0578553c001c7c6f55ad731addd727c40e03a1fe4cc4d832aa527";
 
+const env = process.env.NODE_ENV;
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+  origin:
+    env === "production"
+      ? "https://this-is-me-74cbf.web.app"
+      : "http://localhost:3000",
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 export default async function register(req, res) {
-  const { email = "", password = "", name = "" } = req.body;
-  if (!name || !password || !email) {
-    return res.status(400).json({ message: "Invalid input." });
-  }
-
-  if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
-    return res.status(400).json({ message: "Email is invalid." });
-  }
-
   try {
+    await runMiddleware(req, res, cors);
+    const { email = "", password = "", name = "" } = req.body;
+    if (!name || !password || !email) {
+      return res.status(400).json({ message: "Invalid input." });
+    }
+
+    if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      return res.status(400).json({ message: "Email is invalid." });
+    }
     const client = await clientPromise;
     const db = client.db("user_db");
     const users = db.collection("users");
